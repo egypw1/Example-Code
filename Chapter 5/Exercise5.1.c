d6a9073ccbf960d99eafbe4b091bd027fd773469
egypw1 <egypw1@nottingham.ac.uk>
2024-10-07 10:55:23 +0100

    changes made

--- Chapter 5/Exercise5.1.c
+++ Chapter 5/Exercise5.1.c
@@ -8,7 +8,7 @@ int main(void)
     float f = 1.23f;
 
     // Use printf display text on the screen
-    printf ("The variables are\na = %d\nb=%d\nf=%f", a, b, f);
+    printf ("The variables are...\na = %d\nb=%d\nf=%f", a, b, f);
 
     // Exit from main
     return 0;
